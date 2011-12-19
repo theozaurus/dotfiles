882f92f25c2b9afa4618eab4c40e8cd3d696ed0f
Theo Cushion <theo@jivatechnology.com>
2011-12-19 17:11:09 +0000

    Fix: Added mysql directory to DYLD_LIBRARY_PATH

--- .profile.d/mysql.sh
+++ .profile.d/mysql.sh
@@ -1,1 +1,4 @@
-export PATH=$PATH:/usr/local/mysql/bin
+export PATH="$PATH:/usr/local/mysql/bin"
+
+# http://stackoverflow.com/questions/4512362/rails-server-fails-to-start-with-mysql2-using-rvm-ruby-1-9-2-p0-on-osx-10-6-5
+export DYLD_LIBRARY_PATH="/usr/local/mysql/lib:$DYLD_LIBRARY_PATH"
